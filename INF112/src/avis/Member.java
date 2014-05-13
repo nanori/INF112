fec12ef2cd2717dd15fc959081a512f9bd493d05
nanori <yannick.lucet@gmail.com>
2014-05-13 16:07:42 +0200

    Création de la javadoc de Member

--- INF112/src/avis/Member.java
+++ INF112/src/avis/Member.java
@@ -1,22 +1,45 @@
 package avis;
 
 import java.util.LinkedList;
-
+/**
+ * @author Yannick
+ *
+ */
 public class Member {
 
 	/**
-	 * ATTRIBUTS
+	 * Le pseudo du membre
 	 */
 	private String pseudo;
+	
+	/**
+	 * Mot de passe du membre
+	 */
 	private String password;
+	
+	/**
+	 * Profil du membre
+	 */
 	private String profil;
 	private SocialNetwork socialNetwork = null;
+	
+	/**
+	 * Liste des avis déposés par le membre
+	 */
 	private LinkedList <Avis> avis;
 	
 	/**
-	 * CONSTRUCTEUR
+	 * Initialise les attributs
+	 * 
+	 * @param pseudo
+	 * 			Pseudo du membre
+	 * @param password
+	 * 			Mot de passe du membre
+	 * @param profil
+	 * 			Profil du membre
+	 * @param socialNetwork
+	 * 			Le SocialNetwork
 	 */
-	
 	public Member(String pseudo, String password, String profil, SocialNetwork socialNetwork){
 		this.pseudo=pseudo;
 		this.password=password;
@@ -25,10 +48,19 @@ public class Member {
 		avis = new LinkedList<Avis>();
 	}
 	
-	/**
-	 * METHODES
-	 */
 	
+	/**
+	 * Test si le couple pseudo mot de passe corespond au membre.<br>
+	 * Les espaces avant et apres le titre en parametre sont ignorés. La methode n'est pas sensible à la casse.
+	 * @param pseudo
+	 * 			Pseudo à tester
+	 * @param password
+	 * 			Mot de passe à tester
+	 * 
+	 * @return
+	 * 			Vrai si le couple pseudo mot de passe correspond au membre. <br>
+	 * 			Faux si non
+	 */
 	public boolean exists(String pseudo, String password) {
 		
 		if(this.pseudo.trim().equalsIgnoreCase(pseudo) && this.password==password){
@@ -38,13 +70,29 @@ public class Member {
 		}
 	}
 	
+	/**
+	 * Verifie si le pseudo est celui du membre.
+	 * 
+	 * @param pseudo
+	 * 			Pseudo à tester
+	 * @return
+	 * 			Vrai si le pseudo correspond au membre. <br>
+	 * 			Faux si non
+	 */
 	public boolean exists(String pseudo){
 		return this.pseudo.equalsIgnoreCase(pseudo.trim());
 	}
 
+	/**
+	 * Ajoute un avis à la linkedList avis
+	 * 
+	 * @param avis
+	 * 			Avis à ajouter
+	 */
 	public void addReviewToCollection(Avis avis) {
 		this.avis.addLast(avis);
 	}
+	
 	
 	public String toString(){
 		String retour="Pseudo : " + pseudo + "\n" +
