c30198dee7c0d024f41d94d0a978291292d0551e
nanori <yannick.lucet@gmail.com>
2014-05-13 11:14:48 +0200

    Création de la javadoc Book

--- INF112/src/avis/Book.java
+++ INF112/src/avis/Book.java
@@ -1,16 +1,32 @@
 package avis;
 
 
+/**
+ * @author Yannick LUCET
+ * @author Tom VEILLARD
+ */
 public class Book extends Item {
 
 	/**
-	 * ATTRIBUTS
+	 * Nom de l'auteur du livre
 	 */
 	private String auteur;
+	
+	/**
+	 * Nombre de pages du livre
+	 */
 	private int nbPages;
 	
 	/**
-	 * CONSTRUCTEUR
+	 * Initialise les attributs 
+	 * @param titre
+	 * 			Titre du livre
+	 * @param genre
+	 * 			Genre du livre
+	 * @param auteur
+	 * 			Nom de l'auteur
+	 * @param nbPages
+	 * 			Nombre de pages du livre
 	 */
 	public Book(String titre, String genre, String auteur, int nbPages) {
 		super(titre, genre);
@@ -19,7 +35,10 @@ public class Book extends Item {
 	}
 	
 	/**
-	 * METHODES
+	 * Retoune un string decrivant le livre
+	 * 
+	 * @return
+	 * 			string decrivant le livre
 	 */
 	public String toString(){
 		String retour=super.toString() +
