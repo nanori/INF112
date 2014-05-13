babe3e06f5e71d2c263e7a78cadd94533655c94d
nanori <yannick.lucet@gmail.com>
2014-05-13 11:15:17 +0200

    Création de la javadoc Film

--- INF112/src/avis/Film.java
+++ INF112/src/avis/Film.java
@@ -1,16 +1,38 @@
 package avis;
-
+/**
+ * @author Yannick LUCET
+ * @author Tom VEILLARD
+ */
 public class Film extends Item {
 
 	/**
-	 * ATTRIBUTS
+	 * Nom du réalisateur du film
 	 */
 	private String realisateur;
+	
+	/**
+	 * Nom du scenariste du film
+	 */
 	private String scenariste;
+	
+	/**
+	 * Durée du film en minute
+	 */
 	private int duree;
 
 	/**
-	 * CONSTRUCTEUR
+	 * Initialise les attributs
+	 * 
+	 * @param titre
+	 * 			Titre du film
+	 * @param genre
+	 * 			Genre du film
+	 * @param realisateur
+	 * 			Nom du réalisateur du film
+	 * @param scenariste
+	 * 			Nom du scenariste du film
+	 * @param duree
+	 * 			Durée du film en minute
 	 */
 	public Film(String titre, String genre, String realisateur, String scenariste, int duree) {
 		super(titre, genre);
@@ -20,7 +42,10 @@ public class Film extends Item {
 	}
 
 	/**
-	 * METHODES
+	 * Retoune un string decrivant le film
+	 * 
+	 * @return
+	 * 			string decrivant le film
 	 */
 	public String toString(){
 		String retour=super.toString() +
