ac9f61d7fc1797cd61aff92a9203e367e2fe849c
nanori <yannick.lucet@gmail.com>
2014-05-13 09:47:09 +0200

    Rennomage de la classe TestSocialNetwork --> TestsSocialNetwork

---
+++ INF112/src/test/TestsSocialNetwork.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestsSocialNetwork {
+	public static void main(String [] arg){
+	
+	TestsInitialisation.main(new String [0]);
+	TestsAddMember.main(new String [0]);
+	TestsAddItemBook.main(new String [0]);
+	TestsReviewItemBook.main(new String [0]);
+
+	
+	}
+}
